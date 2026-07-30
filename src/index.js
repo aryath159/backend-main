6bed9f24e5b5958c172feee8297e58e93b3f8936
aryath159 <aryanpise12@gmail.com>
2026-07-30 23:40:47 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -3,18 +3,36 @@ import dotenv from "dotenv"
 import mongoose from "mongoose" ;
 import {DB_NAME} from './constants.js';
 
-
+import express from 'express'
 
 //  one approach - write code in another file 
 import connectDB from "./db/index.js";
 
+const app = express() ;
 
 dotenv.config({
     path: './.env'
 })
 
 
-connectDB() ;
+connectDB() 
+.then(()=>{
+
+    
+    const server = app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is running at port ${process.env.PORT}`) ;
+    })
+
+    // handle server level error
+    server.on("error" , (err)=>{
+        console.log("error : " , err) ;
+        
+    })
+
+})
+.catch((err)=>{
+    console.log("MONGO DB connection failed : " ,  err) ;
+})
 
 
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,41 @@
+// wraper function 
+
+
+const asyncHandler = (requestHandler) => {
+    (req, res , next) => {
+        Promise.resolve( requestHandler(req , res , next))
+        .catch( (err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+
+
+
+
+
+
+
+
+// second method 
+
+// const temp = () => { () =>{} } this and this ->       () => () => {} are equal only curly brace are removed
+
+
+// accepts a function as a parameter and returns a sync function
+
+// const asyncHandler = (fn) => async (req , res , next) => {
+//     try {
+//             await fn(req , res , next)
+//     } catch (error) {
+        
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message: err.message
+//         })
+//     }
+// }
